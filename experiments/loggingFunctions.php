8030b0670f0f7f6c0c5922c747c0027995f65722
Daniel Ho <danielho1001@gmail.com>
2015-07-05 17:45:04 -0700

    some logging

---
+++ experiments/loggingFunctions.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+echo "php file executed";
+http_response_code(404);
+// $user_clicks = fopen("../logs/user_clicks_1.csv", "w") or die("Unable to open file!");
+// fwrite($user_clicks, "testing") or die('fwrite failed');
+// fclose($user_clicks);
+?>
---
+++ js/functions.js
@@ -0,0 +1,3 @@
+function formatLogStatement(type, user, source, result) {
+	return type + "," + user + "," + source + "," + result
+}
